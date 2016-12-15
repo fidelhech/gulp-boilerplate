25d77565de80a5984da66d40c290c4cd29e1488d
Fidel Hechavarria <jamirof@gmail.com>
2016-12-15 11:50:21 -0500

    Autoprefixer support

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 // Gulp Plugins
-var gulp        = require('gulp'),
-    pug         = require('gulp-pug'),
-    browserSync = require('browser-sync').create(),
-    sass        = require('gulp-sass'),
-    sourcemaps  = require('gulp-sourcemaps');
+var gulp          = require('gulp'),
+    pug           = require('gulp-pug'),
+    browserSync   = require('browser-sync').create(),
+    sass          = require('gulp-sass'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    prefix        = require('gulp-autoprefixer');
 
 /**
  * Development Tasks
@@ -35,14 +36,17 @@ gulp.task('pug', function(){
 gulp.task('sass', function(){
   return gulp.src('src/sass/main.sass')
       .pipe(sourcemaps.init())
-      .pipe(sass())
-      .pipe(sourcemaps.write())
+      .pipe(sass({
+        outputStyle: 'compressed'
+      }))
+      .pipe(prefix('last 2 versions'))
+      .pipe(sourcemaps.write('maps'))
       .pipe(gulp.dest('builds/dev/css'))
       .pipe(browserSync.reload({stream: true}));
 });
 
 /* Default development Task */
-gulp.task('dev', ['sass', 'browser-sync', 'watch']);
+gulp.task('dev', ['pug', 'sass', 'browser-sync', 'watch']);
 
 
 
